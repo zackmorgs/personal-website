8a4afb369dfc7735f896092382cd7465eb174f70
Zack Morgenthaler <zmorgenthaler14@my.whitworth.edu>
2018-06-03 12:45:47 -0500

    came to a great understanding of css grid and began implementing a better system with newfound understanding :D

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <React.Fragment>
 
         <nav>
-          <div class='navbar'>
+          <div className='navbar'>
             <button className='nav-toggle'>
               <svg xmlns='http://www.w3.org/2000/svg' width='2rem' height='2rem' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='1' strokeLinecap='round' strokeLinejoin='round' className='feather feather-menu'>
                 <line x1='3' y1='12' x2='21' y2='12' />
