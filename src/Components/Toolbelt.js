c42993ebd20da792ef328214e8b3f66aca4c9570
Zack Morgenthaler <zmorgenthaler14@my.whitworth.edu>
2018-06-09 10:49:18 -0500

    For now I'm going to focus on other problems facing my site.

--- src/Components/Toolbelt.js
+++ src/Components/Toolbelt.js
@@ -1,18 +1,19 @@
 // todo
-Toolbelt => ({
-  CssFriendlyString = function (StringArgument) {
-    var localString = StringArgument;
-    var replaceAll = function (string_argument, target, replacement) {
-      return string_argument.split(target).join(replacement);
-    }
-    localString = String(string_argument);
-    localString = replaceAll(localString,' ','_');
-    localString = replaceAll(localString,'.','_');
-    localString = replaceAll(localString,',','_');
-    localString = replaceAll(localString,'-','_')  ;
-    localString.replace(/[!\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '_');
-    return localString;
-  },
-});
+// Toolbelt => ({
+//   CssFriendlyString (StringArgument) {
+//     var localString = StringArgument;
+//     var replaceAll = function (string_argument, target, replacement) {
+//       return string_argument.split(target).join(replacement);
+//     }
+//     localString = String(string_argument);
+//     localString = replaceAll(localString,' ','_');
+//     localString = replaceAll(localString,'.','_');
+//     localString = replaceAll(localString,',','_');
+//     localString = replaceAll(localString,'-','_')  ;
+//     localString.replace(/[!\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '_');
+//     return localString;
+//   }
 
-export default Toolbelt;
+// });
+
+// export default Toolbelt;
